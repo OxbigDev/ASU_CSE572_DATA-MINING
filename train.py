7632cc29d4e94707465d0a5a0c64e1b206793577
mike_S_home <mike.l.salzarulo@gmail.com>
2021-02-08 18:07:47 -0500

    completed training script

--- train.py
+++ train.py
@@ -6,6 +6,10 @@ import pandas as pd
 import sklearn
 import pickle
 
+from sklearn.neural_network import MLPClassifier
+from sklearn.model_selection import KFold, train_test_split
+import numpy as np
+
 __author__ = "Mike.Salzarulo"
 
 FILES = ["CGMData.csv",
@@ -17,89 +21,154 @@ def main():
 
     # meal time trigger column (tm)
     meal_ticker = "BWZ Carb Input (grams)"
+    # data of interest
+    col_of_interest = 'Sensor Glucose (mg/dL)'
 
-    # Read in the dataframe concatenate date and time cols
-    insulin_pump_df = pd.read_csv(FILES[2], parse_dates=[['Date', 'Time']], na_filter=False)
-    CGM_df = pd.read_csv(FILES[0], parse_dates=[['Date', 'Time']])
-
-    # extract relevent time stamps
-    all_t = insulin_pump_df.loc[insulin_pump_df[meal_ticker] != ""]
-
-    # Find time windows follow three scenarios
-    meal_range = pd.to_timedelta(2, "hours")
-    start_meal_delta = pd.to_timedelta(.5, "hours")
-    other_meal_delta = pd.to_timedelta(1.5, "hours")
-    tm_list = []
-
-    # iterate over the relevent timestamps to get meal times
-    for _,series in all_t[::-1].iterrows():
-        tm = series.Date_Time
-
-        # skip this logic on first iteration
-        if "tp" in dir():
-            # meal data tm+2hrs
-            if tp - tm > meal_range:
-                tm_list.append(tp - start_meal_delta)
-
-            # tp if tp>tm and tp<tm+2hrs
-            elif tp - tm < meal_range:
-                tm_list.append(tp - start_meal_delta)
-
-            # tm+2hrs = tm then tm+1.5hrs to tm+4hrs
-            elif tp - tm == meal_range:
-                tm_list.append(tp+other_meal_delta)
-
-            # return error message to console
-            else:
-                print(f"There is an error @ {series.Date_Time}")
-
-        # assign current time to previous time
-        tp = tm
-    # this assumes the final tm fits meal data tm+2hrs
-    tm_list.append(tm)
-
-    meal_window = pd.to_timedelta(2.5, "hours")
-    no_meal_window = pd.to_timedelta(2, "hours")
+    file_pairs = [[2, 0], [3, 1]]
     meal_data = []
-
-    # get meal and no meal data
-    # meal data is px Px30
-    for time_stamp in tm_list:
-        data = CGM_df.loc[CGM_df.Date_Time > time_stamp]
-        meal_start = data.Date_Time.min()
-        meal_end = meal_start+meal_window
-        data = data.loc[data.Date_Time < meal_end]
-        meal_data.append(data)
-
-    # no meal data is Qx24
     no_meal_data = []
-    max_time_stamp = CGM_df.Date_Time.max()
-    window_start = CGM_df.Date_Time.min()
-    window_end = window_start + no_meal_window
-    last_meal = iter(meal_data)
-    c_meal_data = pd.concat(meal_data)
-    while window_end < max_time_stamp:
 
-        # todo: there is an error somewhere here
-        if window_end not in c_meal_data.Date_Time and window_start not in c_meal_data.Date_Time:
-            data = CGM_df.loc[CGM_df.Date_Time > window_start]
-            data = data.loc[CGM_df.Date_Time < window_end]
-            no_meal_data.append(data)
+    for pair in file_pairs:
+        # Read in the dataframe concatenate date and time cols
+        insulin_pump_df = pd.read_csv(FILES[pair[0]], parse_dates=[['Date', 'Time']], na_filter=False)
+        CGM_df = pd.read_csv(FILES[pair[1]], parse_dates=[['Date', 'Time']], na_filter=False)
+        pd.to_numeric(CGM_df[col_of_interest]).interpolate(inplace=True, limit=3000, limit_direction="both")
 
-        else:
-            window_end = next(last_meal)[-1]
+        # extract relevent time stamps
+        all_t = insulin_pump_df.loc[insulin_pump_df[meal_ticker] != ""]
 
-        window_start = window_end
+        # Find time windows follow three scenarios
+        meal_range = pd.to_timedelta(2, "hours")
+        start_meal_delta = pd.to_timedelta(.5, "hours")
+        other_meal_delta = pd.to_timedelta(1.5, "hours")
+        tm_list = []
+
+        # iterate over the relevent timestamps to get meal times
+        for _, series in all_t[::-1].iterrows():
+            tm = series.Date_Time
+
+            # skip this logic on first iteration
+            if "tp" in dir():
+                # meal data tm+2hrs
+                if tp - tm > meal_range:
+                    tm_list.append(tp - start_meal_delta)
+
+                # tp if tp>tm and tp<tm+2hrs
+                elif tp - tm < meal_range:
+                    tm_list.append(tp - start_meal_delta)
+
+                # tm+2hrs = tm then tm+1.5hrs to tm+4hrs
+                elif tp - tm == meal_range:
+                    tm_list.append(tp+other_meal_delta)
+
+                # return error message to console
+                else:
+                    print(f"There is an error @ {series.Date_Time}")
+
+            # assign current time to previous time
+            tp = tm
+        # this assumes the final tm fits meal data tm+2hrs
+        tm_list.append(tm)
+
+        meal_window = pd.to_timedelta(2.5, "hours")
+        no_meal_window = pd.to_timedelta(2, "hours")
+
+        # get meal and no meal data
+        # meal data is px Px30
+        for time_stamp in tm_list:
+            data = CGM_df.loc[CGM_df.Date_Time > time_stamp]
+            meal_start = data.Date_Time.min()
+            meal_end = meal_start+meal_window
+            data = data.loc[data.Date_Time < meal_end]
+            meal_data.append(data)
+
+        # no meal data is Qx24
+        max_time_stamp = CGM_df.Date_Time.max()
+        window_start = CGM_df.Date_Time.min()
         window_end = window_start + no_meal_window
+        last_meal = iter(meal_data)
+        c_meal_data = pd.concat(meal_data)
+        while window_end < max_time_stamp:
 
-    pd.concat(no_meal_data).to_csv("no_meal_data.csv")
-    pd.concat(meal_data).to_csv("meal_data.csv")
+            if window_end not in c_meal_data.Date_Time and window_start not in c_meal_data.Date_Time:
+                data = CGM_df.loc[CGM_df.Date_Time > window_start]
+                data = data.loc[CGM_df.Date_Time < window_end]
+                no_meal_data.append(data)
 
-        # interpolate
-        # extract features
-        # train SVM
+            else:
+                window_end = next(last_meal)[-1]
+
+            window_start = window_end
+            window_end = window_start + no_meal_window
+
+    # Extract data to expected format
+    no_meal_data = [pd.to_numeric(x[col_of_interest]).values for x in no_meal_data]
+    [print(f" expected 24 got :{len(x)}") for x in no_meal_data if len(x) != 24]
+    meal_data = [pd.to_numeric(x[col_of_interest]).values for x in meal_data]
+    [print(f" expected 30 got :{len(x)}") for x in meal_data if len(x) != 30]
+
+    train(no_meal_data, meal_data)
 
     return
+
+def train(no_meal, meal):
+
+    # filter out incomplete data
+    no_meal = [x for x in no_meal if len(x) == 24 and True not in np.isnan(x)]
+    no_meal = [np.pad(x, (0,6), "constant", constant_values=0) for x in no_meal]
+    meal = [x for x in meal if len(x) == 30 and True not in np.isnan(x)]
+
+    # pack data
+    training_data = no_meal + meal
+
+    # create label vector
+    labels = [0 for x in range(len(no_meal))] + [1 for x in range(len(meal))]
+
+    # Form data splits
+    kf = KFold(n_splits=8, shuffle=True)
+    training_data, testing_data, labels_train, labels_test = train_test_split(training_data,labels,test_size=.1)
+
+    # define vars
+    models = []
+    i=0
+    training_data = np.asarray(training_data)
+    labels_train = np.asarray(labels_train)
+    labels_test = np.asarray(labels_test)
+
+    # begin training
+    for train_i, test_i in kf.split(training_data):
+        train = training_data[train_i], labels_train[train_i]
+        test = training_data[test_i], labels_train[test_i]
+
+        clf = MLPClassifier((8, 3))
+        clf.fit(train[0], train[1])
+        score = clf.score(test[0], test[1])
+        print(f"Model {i} validation acc {score}")
+        models.append(clf)
+
+        i+=1
+
+    # test on each of the models
+    i = 0
+    scores = []
+    fn = "Trained_model.pkl"
+    for model in models:
+
+        score = model.score(testing_data, labels_test)
+        print(f"Model {i} testing acc {score}")
+        i = +1
+
+        if score == 1.0:
+            print(f"Training has saturated saving model")
+            with open(fn, "wb") as fp:
+                pickle.dump(model.get_params(), fp, protocol=3)
+            return
+
+    # Continue Until a 1 is scored
+    train(no_meal, meal)
+
+    return
+
 
 # expecting 231 rows in 1 col
 
