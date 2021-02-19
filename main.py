21e88b792df2940948828e0a2eedc7eb16559f8d
mike_S_home <mike.l.salzarulo@gmail.com>
2021-02-18 22:53:03 -0500

    wip project 3

--- main.py
+++ main.py
@@ -45,53 +45,58 @@ def main():
     # iterate over the relevent timestamps to get meal times
     for _, series in all_t[::-1].iterrows():
         y_val = int(series[meal_ticker])
-        carb_intake_list.append(y_val) if y_val > 0 else None
         tm = series.Date_Time
 
         # skip this logic on first iteration
-        if "tp" in dir():
+        if "tp" in dir() and y_val > 0:
             # meal data tm+2hrs
             if tp - tm > meal_range:
-                tm_list.append(tp - start_meal_delta)
+                time = tp - start_meal_delta
+                tm_list.append(time)
 
             # tp if tp>tm and tp<tm+2hrs
             elif tp - tm < meal_range:
-                tm_list.append(tp - start_meal_delta)
+                time = tp - start_meal_delta
+                tm_list.append(time)
 
             # tm+2hrs = tm then tm+1.5hrs to tm+4hrs
             elif tp - tm == meal_range:
-                tm_list.append(tp+other_meal_delta)
+                time = tp+other_meal_delta
+                tm_list.append(time)
 
             # return error message to console
             else:
                 print(f"There is an error @ {series.Date_Time}")
 
+            carb_intake_list.append((y_val, time))
+
         # assign current time to previous time
         tp = tm
     # this assumes the final tm fits meal data tm+2hrs
-    tm_list.append(tm)
+    # tm_list.append(tm)
 
     # build cluster labels
-    carb_intake_list.sort(key=lambda x: int(x))
+    carb_intake_list.sort(key=lambda x: int(x[0]))
     labels = {}
-    count = 1
-    high_carb = carb_intake_list[0]+20
-    label_name = "A" * count
+    count = 0
+    high_carb = carb_intake_list[0][0]+20
+    label_name = str(count)
+
     for carb in carb_intake_list:
-        if carb < high_carb:
+        if carb[0] < high_carb:
             if label_name in labels:
                 labels[label_name].append(carb)
             else:
                 labels[label_name] = [carb]
         else:
             count += 1
-            high_carb += 20 if high_carb < 100 else 1000
-            label_name = "A" * count
+            high_carb += 20
+            label_name = str(count)
             labels[label_name] = [carb]
-
 
     meal_window = pd.to_timedelta(2.5, "hours")
     no_meal_window = pd.to_timedelta(2, "hours")
+    meal_truth_label = []
 
     # get meal and no meal data
     # meal data is px Px30
@@ -101,48 +106,53 @@ def main():
         meal_end = meal_start+meal_window
         data = data.loc[data.Date_Time < meal_end]
         meal_data.append(data)
-
-    # no meal data is Qx24
-    max_time_stamp = CGM_df.Date_Time.max()
-    window_start = CGM_df.Date_Time.min()
-    window_end = window_start + no_meal_window
-    last_meal = iter(meal_data)
-    c_meal_data = pd.concat(meal_data)
-    while window_end < max_time_stamp:
-
-        if window_end not in c_meal_data.Date_Time and window_start not in c_meal_data.Date_Time:
-            data = CGM_df.loc[CGM_df.Date_Time > window_start]
-            data = data.loc[CGM_df.Date_Time < window_end]
-            no_meal_data.append(data)
-
-        else:
-            window_end = next(last_meal)[-1]
-
-        window_start = window_end
-        window_end = window_start + no_meal_window
+        [meal_truth_label.append(label) for label, items in labels.items() if time_stamp in [x[1] for x in items]]
 
     # Extract data to expected format
-    no_meal_data = [pd.to_numeric(x[col_of_interest]).values for x in no_meal_data]
-    [print(f" expected 24 got :{len(x)}") for x in no_meal_data if len(x) != 24]
     meal_data = [pd.to_numeric(x[col_of_interest]).values for x in meal_data]
     [print(f" expected 30 got :{len(x)}") for x in meal_data if len(x) != 30]
 
     # filter out incomplete data
-    no_meal = [x for x in no_meal_data if len(x) == 24 and True not in np.isnan(x)]
-    meal = np.asarray([x[6:] for x in meal_data if len(x) == 30 and True not in np.isnan(x)])
+    meal_ind = np.asarray([x for x, y in enumerate(meal_data) if len(y) == 30 and True not in np.isnan(y)])
 
-    meal_max = np.max(meal)
-    meal_min = np.min(meal)
+    # apply filter
+    meal = np.asarray(meal_data)[meal_ind]
+    meal_truth_label = np.asarray(meal_truth_label)[meal_ind]
 
     # run k means on meal data
     kmeans_model = KMeans(n_clusters=6)
-    kmeans_model = kmeans_model.fit(meal)
+    kmeans_model = kmeans_model.fit(meal.tolist())
+    ky = kmeans_model.labels_
 
     # run DBSCAN on meal data
     dbscan_model = DBSCAN()
-    dbscan_model.fit(meal)
+    dbscan_model.fit(meal.tolist())
+    dby = dbscan_model.labels_
+
+    # create confusion matrix
+    confusion_matrix = build_confusion_matrix(ky, meal_truth_label, labels)
+    cluster_means = np.mean(kmeans_model.cluster_centers_, axis=1)
+
+    [sse(x, y) for x, y in zip(confusion_matrix, cluster_means)]
 
     return
+
+def build_confusion_matrix(ky, meal_truth_label):
+    p_labels = np.zeros((ky.max()+1, meal_truth_label.max()+1))
+
+    for y, truth in zip(ky, meal_truth_label.astype(np.uint8)):
+        p_labels[y, truth] += 1
+
+    return p_labels
+
+def sse(cluster, mean):
+    return sum(map(lambda x: (x-mean)**2, cluster))
+
+def purity():
+    return purity
+
+def entropy():
+    return entropy
 
 
 if __name__ == '__main__':
